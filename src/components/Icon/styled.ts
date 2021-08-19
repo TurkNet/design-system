5b42af78af52f0b16aacb27b8be3527a32809085
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2021-08-19 10:11:28 +0300

    Icon flicker fix

--- src/components/Icon/styled.ts
+++ src/components/Icon/styled.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components'
 import { typography, TypographyProps, color, ColorProps } from 'styled-system'
+import { fadeIn } from '../../shared/animation'
+import { prop } from '../../utility'
 
 export type IconStyledProps = TypographyProps & ColorProps
 
 export const IconStyled = styled.span<IconStyledProps>`
   ${typography}
   ${color}
+  width: ${prop('fontSize')}px;
+  overflow: hidden;
+  display: block;
+  animation: ${fadeIn} 0.5s ease;
 `
