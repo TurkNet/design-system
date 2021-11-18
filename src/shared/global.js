08b439fa6534405dc484ebba95df53eb253425ce
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2021-11-18 14:22:01 +0300

    Use normalize

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,8 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
+import normalize from 'normalize.css'
 import { theme } from './theme'
 
 export const GlobalStyle = createGlobalStyle`
+${normalize}
  body {
+  margin: 0;
+  padding: 0;
   font-family: 'Open Sans', sans-serif;
   font-size: ${theme.fontSizes[14]};
   color: ${theme.colors.grey['800']};
