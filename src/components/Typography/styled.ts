ce40c6d453382e266c8daac1040ec0a95e1c428f
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2021-12-13 18:15:49 +0300

    lLine-height fix (#211)

--- src/components/Typography/styled.ts
+++ src/components/Typography/styled.ts
@@ -13,64 +13,51 @@ import { prop } from '../../utility/styled'
 const variants = {
   h1: {
     fontSize: '36',
-    lineHeight: '48px',
     fontWeight: 'semi-bold',
   },
   h2: {
     fontSize: '32',
-    lineHeight: '40px',
     fontWeight: 'semi-bold',
   },
   h3: {
     fontSize: '30',
-    lineHeight: '40px',
     fontWeight: 'bold',
   },
   h4: {
     fontSize: '26',
-    lineHeight: '32px',
     fontWeight: 'bold',
   },
   h5: {
     fontSize: '22',
-    lineHeight: '32px',
     fontWeight: 'semi-bold',
   },
   h6: {
     fontSize: '18',
-    lineHeight: '24px',
     fontWeight: 'semi-bold',
   },
   subtitle1: {
     fontSize: '15',
-    lineHeight: '24px',
     fontWeight: 'semi-bold',
   },
   subtitle2: {
     fontSize: '13',
-    lineHeight: '24px',
     fontWeight: 'semi-bold',
   },
   paragraph1: {
     fontSize: '15',
-    lineHeight: '20px',
   },
   paragraph2: {
     fontSize: '13',
-    lineHeight: '18px',
   },
   caption1: {
     fontSize: '12',
-    lineHeight: '16px',
   },
   caption2: {
     fontSize: '12',
-    lineHeight: '16px',
     fontWeight: 'semi-bold',
   },
   label: {
     fontSize: '12',
-    lineHeight: '16px',
     fontWeight: 'bold',
   },
 }
