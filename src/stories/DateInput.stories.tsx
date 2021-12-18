cf5b5288a4c900c36774b7b639f3d448a4026a5a
Emre Birkan Kervan <kervan95@gmail.com>
2021-12-18 22:49:04 +0300

    added daterangeInout seperate component

--- src/stories/DateInput.stories.tsx
+++ src/stories/DateInput.stories.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Story } from '@storybook/react'
+import { DateRangeInput } from 'src/components/DateRangeInput'
 import { DateInput, DateInputProps } from '../components'
 
 export default {
@@ -8,14 +9,27 @@ export default {
 }
 
 const Template: Story<DateInputProps> = args => {
+  const dateFormat = 'dd.MM.yyyy'
   const [date, setDate] = useState<any>(new Date())
+  const [rangeDate, setRangeDate] = useState<any>(new Date())
+
   return (
-    <DateInput
-      {...args}
-      selected={date}
-      onChange={date => setDate(date)}
-      variant="danger"
-    />
+    <>
+      <DateInput {...args} selected={date} onChange={date => setDate(date)} />
+      <br />
+      <br />
+      <br />
+
+      <DateRangeInput
+        {...args}
+        dateFormat={dateFormat}
+        name="dateRange"
+        placeholderText="Tarih aralığı seçiniz"
+        selected={rangeDate as any}
+        onChange={rangeDate => setRangeDate(rangeDate)}
+        selectsRange
+      />
+    </>
   )
 }
 
