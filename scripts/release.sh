62a9ba2cfd058863d879f6fa520ed2f291242302
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2021-12-28 02:39:14 +0300

    refactor release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,6 @@
 export PATH=$(npm bin):$PATH
 
 VERSION=`auto version`
-export GH_TOKEN=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
-export NPM_TOKEN=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
 
 ## Support for label 'skip-release'
 if [ ! -z "$VERSION" ]; then
