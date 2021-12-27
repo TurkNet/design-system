842887a63106e2f412288566dc15996f19c2b647
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2021-12-28 00:31:11 +0300

    release config

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+export PATH=$(npm bin):$PATH
+
+VERSION=`auto version`
+
+## Support for label 'skip-release'
+if [ ! -z "$VERSION" ]; then
+  ## Update Changelog
+  auto changelog
+
+  ## Publish Package
+  npm version $VERSION -m "Bump version to: %s [skip ci]"
+  cd dist
+  npm publish
+
+  ## Create GitHub Release
+  git push --follow-tags --set-upstream origin $branch
+  auto release --base-branch=main
+fi
