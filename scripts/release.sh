d776a2f605120a68c7573cffbced9b05e1512a2d
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2022-01-04 19:35:06 +0300

    release config

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,6 @@ export PATH=$(npm bin):$PATH
 
 VERSION=`auto version`
 
-export GH_TOKEN=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
-export NPM_TOKEN=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
-
 ## Support for label 'skip-release'
 if [ ! -z "$VERSION" ]; then
   ## Update Changelog
