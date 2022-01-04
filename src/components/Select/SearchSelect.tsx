92dd37a254ea8ff2d28203202c6acfd493c3dd4d
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2022-01-04 18:51:18 +0300

    React Selact (#262)

--- src/components/Select/SearchSelect.tsx
+++ src/components/Select/SearchSelect.tsx
@@ -1,124 +1,72 @@
-import React, { ReactNode, useState, useEffect } from 'react'
-import {
-  SelectStyled,
-  InputStyled,
-  OverlayStyled,
-  OptionStyled,
-  BgStyled,
-} from './styled'
-import { Icon } from '../Icon'
-import { noop } from '../../utility'
-import type { InputProps } from '../Input'
+import React from 'react'
+import { components } from 'react-select'
+import { SearchSelectStyled } from './styled'
 
-type IOption = Record<string, any>
+type IOption = Promise<Record<string, string>>
 
-export type SearchSelectProps = {
+export type SearchReactSelectProps = {
+  name?: string
   labelKey?: string
   valueKey?: string
-  value?: string
-  name?: string
-  options: IOption[]
-  locale?: string
-  icon?: ReactNode
-  onSelect(value: IOption | undefined): void
-  onSearch(value: string): void
-  onToggle?(open: boolean): void
-} & Omit<InputProps, 'onSelect' | 'value'>
+  options?: IOption[]
+  isSearchable?: boolean
+  isMulti?: boolean
+  placeholder?: string
+  variant?: 'success' | 'info' | 'danger' | 'warning' | 'primary' | undefined
+  icon?: React.ReactNode
+  onChange?(newValue: unknown, meta?: any): void
+  onSearch(inputValue: unknown): Promise<IOption[]>
+}
 
-export const SearchSelect = React.forwardRef<
-  HTMLInputElement,
-  SearchSelectProps
->(
-  (
-    {
-      name = 'select',
-      options = [],
-      labelKey = 'label',
-      valueKey = 'id',
-      value = '',
-      placeholder = 'Seçiniz',
-      icon,
-      onSelect = noop,
-      onToggle = noop,
-      onSearch = noop,
-      ...props
-    },
-    ref
+let timeout: any = -1
+
+export const SearchSelect = ({
+  options = [],
+  isSearchable = true,
+  isMulti = false,
+  placeholder = 'Seçiniz',
+  variant = 'primary',
+  labelKey = 'label',
+  valueKey = 'id',
+  onSearch,
+  icon,
+  ...props
+}: SearchReactSelectProps) => {
+  const DropdownIndicator = props => (
+    <components.DropdownIndicator {...props}>
+      {icon}
+    </components.DropdownIndicator>
+  )
+
+  const loadOptions = (
+    inputValue: string,
+    callback: (options: IOption[]) => void
   ) => {
-    const [open, setOpen] = useState(false)
-    const [searchValue, setSearchValue] = useState(value)
-    const [selected, setSelected] = useState<IOption>({})
-
-    const handleToggle = () => {
-      setOpen(!open)
-      onToggle(!open)
+    if (timeout) {
+      clearTimeout(timeout)
     }
-
-    const onClose = (e: MouseEvent) => {
-      e.stopPropagation()
-      e.preventDefault()
-      setOpen(false)
-      onToggle(false)
-      onSelect(selected)
-    }
-
-    const handleSelect = (option: IOption) => {
-      setSearchValue(option[labelKey])
-      setSelected(option)
-      onSelect(option)
-      handleToggle()
-    }
-
-    useEffect(() => {
-      setSearchValue(value)
-      setSelected({})
-    }, [value])
-
-    const show = open && options.length > 0
-
-    return (
-      <SelectStyled>
-        <InputStyled
-          {...props}
-          type="select"
-          value={searchValue || ''}
-          onChange={e => onSearch(e.target.value)}
-          placeholder={placeholder}
-          autoComplete="off"
-          onClick={handleToggle}
-          searchable
-          icon={
-            icon || (
-              <Icon
-                name={open ? 'expand_less' : 'expand_more'}
-                color="sky.dark"
-              />
-            )
-          }
-        />
-        <input
-          name={name}
-          ref={ref}
-          type="hidden"
-          defaultValue={selected[valueKey] || ''}
-        />
-        <BgStyled show={show} onClick={onClose} />
-        <OverlayStyled show={show}>
-          {options.map(option => (
-            <OptionStyled
-              key={option[labelKey]}
-              active={value && option[labelKey] === value}
-              onClick={e => {
-                e.stopPropagation()
-                e.preventDefault()
-                handleSelect(option)
-              }}
-            >
-              {option[labelKey]}
-            </OptionStyled>
-          ))}
-        </OverlayStyled>
-      </SelectStyled>
-    )
+    timeout = setTimeout(async () => {
+      callback(await onSearch(inputValue))
+    }, 300)
   }
-)
+
+  return (
+    <SearchSelectStyled
+      components={icon ? { DropdownIndicator } : {}}
+      cacheOptions
+      loadOptions={loadOptions}
+      defaultOptions={options}
+      classNamePrefix="select"
+      isSearchable={isSearchable}
+      isMulti={isMulti}
+      placeholder={placeholder}
+      variant={variant}
+      noOptionsMessage={() => 'Kayıt Bulunamadı.'}
+      loadingMessage={() => 'Yükleniyor..'}
+      closeMenuOnSelect={!isMulti}
+      getOptionLabel={(o: any) => o[labelKey]}
+      getOptionValue={(o: any) => o[valueKey]}
+      {...props}
+    />
+  )
+}
