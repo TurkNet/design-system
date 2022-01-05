eb0941a213b9bad6e15579f4d28859e1920f97dc
Emre Birkan Kervan <kervan95@gmail.com>
2022-01-05 16:22:42 +0300

    added again breadcrumb

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -1,23 +1,11 @@
 import React, { FC } from 'react'
 import BreadcrumbItem from './BreadcrumbItem'
 import { BreadCrumbStyled } from './styled'
-import { Icon } from '../Icon'
 
-export interface BreadcrumbProps {
-  homeIcon?: boolean
-}
-
-const Breadcrumb: FC<BreadcrumbProps> = ({ homeIcon, children }) => {
-  return (
-    <BreadCrumbStyled>
-      {homeIcon && (
-        <BreadcrumbItem homeIcon={homeIcon}>
-          <Icon name="home" />
-        </BreadcrumbItem>
-      )}
-      {children}
-    </BreadCrumbStyled>
-  )
-}
+const Breadcrumb: FC = ({ children }) => (
+  <nav aria-label="breadcrumbs">
+    <BreadCrumbStyled>{children}</BreadCrumbStyled>
+  </nav>
+)
 
 export { Breadcrumb, BreadcrumbItem }
