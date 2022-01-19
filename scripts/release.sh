b1ae65f42a481896162e5f17dd49483b70a0dd2f
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2022-01-19 12:06:31 +0300

    release v1.0.12

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,10 +7,10 @@ if [ ! -z "$VERSION" ]; then
   ## Update Changelog
   auto changelog
 
-  ## Publish Package
-  npm version $VERSION -m "Bump version to: %s [skip ci]"
   yarn build-library
   cd dist
+  ## Publish Package
+  npm version $VERSION -m "Bump version to: %s [skip ci]"
   npm publish
   cd ..
 
