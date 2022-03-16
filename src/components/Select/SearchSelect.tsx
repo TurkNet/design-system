b3e06a2141074e4ec17e856faf4f9396ed2d6778
Emre Birkan Kervan <49693077+birkankervan@users.noreply.github.com>
2022-03-16 15:22:24 +0300

    Revert "Added forwardRef to Select" (#330)

--- src/components/Select/SearchSelect.tsx
+++ src/components/Select/SearchSelect.tsx
@@ -1,82 +1,75 @@
-import React, { SelectHTMLAttributes, ReactNode } from 'react'
-import { components, Props as ReactSelectProps } from 'react-select'
+import React from 'react'
+import { components } from 'react-select'
 import { SearchSelectStyled } from './styled'
 
 type IOption = Promise<Record<string, any>>
 
-export type SearchSelectProps = ReactSelectProps &
-  SelectHTMLAttributes<HTMLSelectElement> & {
-    name?: string
-    value?: any
-    labelKey?: string
-    valueKey?: string
-    options?: IOption[]
-    isSearchable?: boolean
-    isMulti?: boolean
-    placeholder?: string
-    variant?: 'success' | 'info' | 'danger' | 'warning' | 'primary' | undefined
-    icon?: ReactNode
-    isLoading?: boolean
-    isDisabled?: boolean
-    onChange?(newValue: unknown, meta?: any): void
-    onSearch(inputValue: unknown): Promise<IOption[]>
-  }
+export type SearchSelectProps = {
+  name?: string
+  value?: any
+  labelKey?: string
+  valueKey?: string
+  options?: IOption[]
+  isSearchable?: boolean
+  isMulti?: boolean
+  placeholder?: string
+  variant?: 'success' | 'info' | 'danger' | 'warning' | 'primary' | undefined
+  icon?: React.ReactNode
+  isLoading?: boolean
+  isDisabled?: boolean
+  onChange?(newValue: unknown, meta?: any): void
+  onSearch(inputValue: unknown): Promise<IOption[]>
+}
 
 let timeout: any = -1
 
-export const SearchSelect = React.forwardRef<any, SearchSelectProps>(
-  (
-    {
-      options = [],
-      isSearchable = true,
-      isMulti = false,
-      placeholder = 'Seçiniz',
-      variant = 'primary',
-      labelKey = 'label',
-      valueKey = 'id',
-      onSearch,
-      icon,
-      ...props
-    },
-    ref
+export const SearchSelect = ({
+  options = [],
+  isSearchable = true,
+  isMulti = false,
+  placeholder = 'Seçiniz',
+  variant = 'primary',
+  labelKey = 'label',
+  valueKey = 'id',
+  onSearch,
+  icon,
+  ...props
+}: SearchSelectProps) => {
+  const DropdownIndicator = props => (
+    <components.DropdownIndicator {...props}>
+      {icon}
+    </components.DropdownIndicator>
+  )
+
+  const loadOptions = (
+    inputValue: string,
+    callback: (options: IOption[]) => void
   ) => {
-    const DropdownIndicator = props => (
-      <components.DropdownIndicator {...props}>
-        {icon}
-      </components.DropdownIndicator>
-    )
-
-    const loadOptions = (
-      inputValue: string,
-      callback: (options: IOption[]) => void
-    ) => {
-      if (timeout) {
-        clearTimeout(timeout)
-      }
-      timeout = setTimeout(async () => {
-        callback(await onSearch(inputValue))
-      }, 300)
+    if (timeout) {
+      clearTimeout(timeout)
     }
-
-    return (
-      <SearchSelectStyled
-        components={icon ? { DropdownIndicator } : {}}
-        cacheOptions
-        loadOptions={loadOptions}
-        defaultOptions={options}
-        classNamePrefix="select"
-        isSearchable={isSearchable}
-        isMulti={isMulti}
-        placeholder={placeholder}
-        variant={variant}
-        noOptionsMessage={() => 'Kayıt Bulunamadı.'}
-        loadingMessage={() => 'Yükleniyor..'}
-        closeMenuOnSelect={!isMulti}
-        getOptionLabel={(o: any) => o[labelKey]}
-        getOptionValue={(o: any) => o[valueKey]}
-        {...props}
-        ref={ref}
-      />
-    )
+    timeout = setTimeout(async () => {
+      callback(await onSearch(inputValue))
+    }, 300)
   }
-)
+
+  return (
+    <SearchSelectStyled
+      components={icon ? { DropdownIndicator } : {}}
+      cacheOptions
+      loadOptions={loadOptions}
+      defaultOptions={options}
+      classNamePrefix="select"
+      isSearchable={isSearchable}
+      isMulti={isMulti}
+      placeholder={placeholder}
+      variant={variant}
+      noOptionsMessage={() => 'Kayıt Bulunamadı.'}
+      loadingMessage={() => 'Yükleniyor..'}
+      closeMenuOnSelect={!isMulti}
+      getOptionLabel={(o: any) => o[labelKey]}
+      getOptionValue={(o: any) => o[valueKey]}
+      {...props}
+    />
+  )
+}
--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,80 +1,71 @@
-import React, { SelectHTMLAttributes, ReactNode } from 'react'
-import { components, Props as ReactSelectProps } from 'react-select'
+import React from 'react'
+import { components } from 'react-select'
 import { SelectStyled } from './styled'
 
 type IOption = Record<string, any>
 
-export type SelectProps = ReactSelectProps &
-  SelectHTMLAttributes<HTMLSelectElement> & {
-    name?: string
-    value?: any
-    labelKey?: string
-    valueKey?: string
-    options?: IOption[]
-    isSearchable?: boolean
-    isMulti?: boolean
-    placeholder?: string
-    variant?: 'success' | 'info' | 'danger' | 'warning' | 'primary' | undefined
-    icon?: ReactNode
-    locale?: string
-    isLoading?: boolean
-    isDisabled?: boolean
-    onChange?(newValue: unknown, meta?: any): void
-  }
+export type SelectProps = {
+  name?: string
+  value?: any
+  labelKey?: string
+  valueKey?: string
+  options?: IOption[]
+  isSearchable?: boolean
+  isMulti?: boolean
+  placeholder?: string
+  variant?: 'success' | 'info' | 'danger' | 'warning' | 'primary' | undefined
+  icon?: React.ReactNode
+  locale?: string
+  isLoading?: boolean
+  isDisabled?: boolean
+  onChange?(newValue: unknown, meta?: any): void
+}
 
-export const Select = React.forwardRef<any, SelectProps>(
-  (
-    {
-      options = [],
-      isSearchable = true,
-      isMulti = false,
-      placeholder = 'Seçiniz',
-      variant = 'primary',
-      labelKey = 'label',
-      valueKey = 'id',
-      locale = 'tr-TR',
-      icon,
-      ...props
-    },
-    ref
-  ) => {
-    const DropdownIndicator = props => (
-      <components.DropdownIndicator {...props}>
-        {icon}
-      </components.DropdownIndicator>
-    )
+export const Select = ({
+  options = [],
+  isSearchable = true,
+  isMulti = false,
+  placeholder = 'Seçiniz',
+  variant = 'primary',
+  labelKey = 'label',
+  valueKey = 'id',
+  locale = 'tr-TR',
+  icon,
+  ...props
+}: SelectProps) => {
+  const DropdownIndicator = props => (
+    <components.DropdownIndicator {...props}>
+      {icon}
+    </components.DropdownIndicator>
+  )
 
-    const trToEng = (text: string) =>
-      text
-        .toLocaleLowerCase(locale)
-        .replace(/ğ/g, 'g')
-        .replace(/ü/g, 'u')
-        .replace(/ş/g, 's')
-        .replace(/ı/g, 'i')
-        .replace(/ö/g, 'o')
-        .replace(/ç/g, 'c')
+  const trToEng = (text: string) =>
+    text
+      .toLocaleLowerCase(locale)
+      .replace(/ğ/g, 'g')
+      .replace(/ü/g, 'u')
+      .replace(/ş/g, 's')
+      .replace(/ı/g, 'i')
+      .replace(/ö/g, 'o')
+      .replace(/ç/g, 'c')
 
-    return (
-      <>
-        <SelectStyled
-          classNamePrefix="select"
-          isSearchable={isSearchable}
-          isMulti={isMulti}
-          options={options}
-          placeholder={placeholder}
-          variant={variant}
-          components={icon ? { DropdownIndicator } : {}}
-          noOptionsMessage={() => 'Kayıt Bulunamadı.'}
-          closeMenuOnSelect={!isMulti}
-          getOptionLabel={(o: any) => o[labelKey]}
-          getOptionValue={(o: any) => o[valueKey]}
-          filterOption={(opt, inputValue) =>
-            trToEng(String(opt.label)).includes(trToEng(String(inputValue)))
-          }
-          {...props}
-          ref={ref}
-        />
-      </>
-    )
-  }
-)
+  return (
+    <SelectStyled
+      classNamePrefix="select"
+      isSearchable={isSearchable}
+      isMulti={isMulti}
+      options={options}
+      placeholder={placeholder}
+      variant={variant}
+      components={icon ? { DropdownIndicator } : {}}
+      noOptionsMessage={() => 'Kayıt Bulunamadı.'}
+      closeMenuOnSelect={!isMulti}
+      getOptionLabel={(o: any) => o[labelKey]}
+      getOptionValue={(o: any) => o[valueKey]}
+      filterOption={(opt, inputValue) =>
+        trToEng(String(opt.label)).includes(trToEng(String(inputValue)))
+      }
+      {...props}
+    />
+  )
+}
