5f4d1ef37cfec561c6769a98c1fd0da1e25a5a55
Cüneyt Aşkın <cuneyt.askin9@gmail.com>
2023-04-27 16:33:52 +0300

    created reminder comp.

---
+++ src/stories/Reminder.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import { Reminder, ReminderProps } from '../components'
+
+export default {
+  title: 'Design System/Reminder',
+  component: Reminder,
+}
+
+const Template: Story<ReminderProps> = args => {
+  const { colorTone, severity } = args
+  return (
+    <Reminder {...args}>
+      I am a {colorTone} {severity} Reminder. Also I have lots of variants
+    </Reminder>
+  )
+}
+
+export const Standart = Template.bind({})
+
+Standart.args = {
+  severity: 'success',
+  colorTone: 'light',
+}
+
+Standart.argTypes = {
+  severity: {
+    control: {
+      type: 'inline-radio',
+      options: ['success', 'warning', 'danger', 'info'],
+    },
+  },
+  colorTone: {
+    control: {
+      type: 'inline-radio',
+      options: ['light', 'normal', 'dark'],
+    },
+  },
+}
