a37b6fb7c26ec672f6f900951bbc8ce5b9ea177d
Cüneyt Aşkın <cuneyt.askin9@gmail.com>
2023-05-31 21:31:21 +0300

    fix reminder icon round

--- src/components/Reminder/index.tsx
+++ src/components/Reminder/index.tsx
@@ -5,6 +5,7 @@ import { ReminderStyled, ReminderStyledProps } from './styled'
 export interface ReminderProps extends ReminderStyledProps {
   iconName?: string
   iconSize?: number
+  iconClassType?: 'fill' | 'outlined' | 'round'
 }
 
 export const Reminder: FC<ReminderProps> = ({
@@ -12,13 +13,19 @@ export const Reminder: FC<ReminderProps> = ({
   colorTone = 'normal',
   iconName = 'warning',
   iconSize = 24,
+  iconClassType,
   children,
   ...props
 }) => {
   const iconColor = `${severity}.${colorTone}`
   return (
     <ReminderStyled severity={severity} colorTone={colorTone} {...props}>
-      <Icon name={iconName} size={iconSize} color={iconColor} />
+      <Icon
+        name={iconName}
+        size={iconSize}
+        color={iconColor}
+        iconClassType={iconClassType}
+      />
       <div>{children}</div>
     </ReminderStyled>
   )
